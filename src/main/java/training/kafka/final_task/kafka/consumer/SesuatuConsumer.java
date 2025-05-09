06ca678745243f6d7dc153934c00c6b57837d959
whatsmynameidontknow <akunhantusaya@gmail.com>
2025-05-09 09:45:23 +0700

    refactor: pindahin biar lebih rapi

--- src/main/java/training/kafka/final_task/kafka/consumer/SesuatuConsumer.java
+++ src/main/java/training/kafka/final_task/kafka/consumer/SesuatuConsumer.java
@@ -4,11 +4,11 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.RetryableTopic;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
 import training.kafka.final_task.avro.Sesuatu;
+import training.kafka.final_task.kafka.producer.SesuatuProducer;
 import training.kafka.final_task.service.SesuatuService;
 import training.kafka.final_task.util.SesuatuUtil;
 
@@ -23,7 +23,7 @@ public class SesuatuConsumer {
     private SimpMessagingTemplate simpMessagingTemplate;
 
     @Autowired
-    private KafkaTemplate<Void, Void> kafkaTemplate;
+    private SesuatuProducer sesuatuProducer;
 
     @RetryableTopic(attempts = "1")
     @KafkaListener(topics = "${topic.sesuatu.name}", groupId = "group-01")
@@ -40,7 +40,7 @@ public class SesuatuConsumer {
             Double result = sesuatuService.calculateAndSave(data);
             System.out.printf("Result of %s = %f%n", SesuatuUtil.stringRepresentationOf(data),
                     result);
-            kafkaTemplate.send("results", null);
+            sesuatuProducer.send();
         } catch (Exception e) {
             log.error("Error processing message: {}", e.getMessage(), e);
             throw e;
--- src/main/java/training/kafka/final_task/kafka/producer/SesuatuProducer.java
+++ src/main/java/training/kafka/final_task/kafka/producer/SesuatuProducer.java
@@ -10,15 +10,22 @@ import training.kafka.final_task.avro.Sesuatu;
 public class SesuatuProducer {
     private final KafkaTemplate<Long, Sesuatu> template;
 
-    @Value("${topic.sesuatu.name}")
-    private String topicName;
+    private final KafkaTemplate<Void, Void> anotherTemplate;
 
-    SesuatuProducer(KafkaTemplate<Long, Sesuatu> template) {
+    @Value("${topic.sesuatu.name}")
+    private String sesuatuTopicName;
+
+    @Value("${topic.results.name}")
+    private String resultsTopicName;
+
+    SesuatuProducer(KafkaTemplate<Long, Sesuatu> template,
+            KafkaTemplate<Void, Void> anotherTemplate) {
         this.template = template;
+        this.anotherTemplate = anotherTemplate;
     }
 
     public void send(Sesuatu sesuatu) {
-        var sent = template.send(topicName, (new Date()).getTime(), sesuatu);
+        var sent = template.send(sesuatuTopicName, (new Date()).getTime(), sesuatu);
         sent.whenComplete((res, ex) -> {
             if (null == ex) {
                 System.out.printf("Message sent = Sesuatu(numbers = %v, operation = %v)%n",
@@ -27,6 +34,17 @@ public class SesuatuProducer {
                 System.out.printf(
                         "Failed to send message = Sesuatu(numbers = %v, operation = %v)%n",
                         sesuatu.getNumbers(), sesuatu.getOperation());
+            }
+        });
+    }
+
+    public void send() {
+        var sent = anotherTemplate.send(resultsTopicName, null);
+        sent.whenComplete((res, ex) -> {
+            if (null == ex) {
+                System.out.printf("Successfully sent to %s%n", resultsTopicName);
+            } else {
+                System.out.printf("Failed to send message to %s%n", resultsTopicName);
             }
         });
     }
